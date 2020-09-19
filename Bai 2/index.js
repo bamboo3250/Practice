70e554b8f544bc861b4a40853c7ad9f130c7cd97
DESKTOP-77CKOS0\tu_an <hson20052001@gmail.com>
2020-09-19 23:21:48 +0700

    submit bài 2 II

--- Bai 2/index.js
+++ Bai 2/index.js
@@ -4,12 +4,12 @@
 	let b = 10;
 
 	var result = "";
-	if(a > b){
+	if (a > b) {
 		result = a
 	}
-	if(b > a){
+	if (b > a) {
 		result = b
-	}if(a == b){
+	} if (a == b) {
 		result = a
 	}
 	// gán số lớn hơn vào result
@@ -23,6 +23,14 @@
 	let list = [3, 2, 4, 5];
 
 	var result = 0;
+	for (let i = 0; i < list.length; i++) {
+		if (result < list[i]) {
+			result = list[i]
+		}
+		console.log(result)
+		console.log(list[i])
+
+	}
 	// gán số lớn nhất vào result
 
 	console.log(`số lớn hơn trong dãy số ${list} là ${result}`);	// in ra số nhỏ nhất
@@ -31,9 +39,16 @@
 {
 	// Cho 2 số a và b. In ra số nhỏ hơn 
 	let a = 3;
-	let b = 2;
+	let b = 3;
 
 	var result = 0;
+
+	if (a < b) {
+		result = a
+	} else {
+		result = b
+	}
+
 	// gán số nhỏ hơn vào result
 
 	console.log(`số nhỏ hơn giữa ${a} và ${b} là ${result}`);	// in ra số nhỏ hơn
@@ -43,8 +58,16 @@
 	// Cho dãy số nguyên. In ra số nhỏ nhất trong dãy số.
 	let list = [3, 2, 4, 5];
 
-	var result = 0;
+	var result = "";
 	// gán số nhỏ nhất vào result
+	for (let i = 0; i < list.length; i++) {
+		if (result < list[i]) {
+			result = list[i]
+			
+		}
+
+
+	}
 
 	console.log(`số nhỏ hơn trong dãy số ${list} là ${result}`);	// in ra số nhỏ nhất
 }
