fb83645a6ce0436b96717c980d5a32c293d9c753
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2020-09-19 23:32:24 +0800

    Bai 2

---
+++ Bai 2/index.js
@@ -0,0 +1,44 @@
+{
+	// Cho 2 số a và b. In ra số lớn hơn 
+	let a = 10;
+	let b = 5;
+
+	var result = 0;
+	// gán số lớn hơn vào result
+
+
+	console.log(`số lớn hơn giữa ${a} và ${b} là ${result}`);	// in ra số lớn hơn
+}
+
+{
+	// Cho dãy số nguyên. In ra số lớn nhất trong dãy số.
+	let list = [3, 2, 4, 5];
+
+	var result = 0;
+	// gán số lớn nhất vào result
+
+	console.log(`số lớn hơn trong dãy số ${list} là ${result}`);	// in ra số nhỏ nhất
+}
+
+{
+	// Cho 2 số a và b. In ra số nhỏ hơn 
+	let a = 3;
+	let b = 2;
+
+	var result = 0;
+	// gán số nhỏ hơn vào result
+
+	console.log(`số nhỏ hơn giữa ${a} và ${b} là ${result}`);	// in ra số nhỏ hơn
+}
+
+{
+	// Cho dãy số nguyên. In ra số nhỏ nhất trong dãy số.
+	let list = [3, 2, 4, 5];
+
+	var result = 0;
+	// gán số nhỏ nhất vào result
+
+	console.log(`số nhỏ hơn trong dãy số ${list} là ${result}`);	// in ra số nhỏ nhất
+}
+
+
