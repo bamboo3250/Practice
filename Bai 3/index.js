35ee83ee3108f2a08fbb9d9f136743a3c52e227c
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2020-09-21 22:50:36 +0800

    Add bai 3

---
+++ Bai 3/index.js
@@ -0,0 +1,6 @@
+function findOppositeNumber(n, inputNumber) {
+	return 0;
+}
+
+console.log(`findOppositeNumber(10, 2) = ${findOppositeNumber(10, 2)}`);
+console.log(`findOppositeNumber(10, 6) = ${findOppositeNumber(10, 6)}`);
